cf93cac6f586537e81a0189d0371e5c56734bf8e
Corinna MacIntyre <144712136+corinnamacintyre@users.noreply.github.com>
2025-11-12 23:52:15 +0000

    testing

--- script.py
+++ script.py
@@ -1,4 +1,9 @@
-id_number = A1111
-password = B1111
+id_number = '1111'
+incorrect_id = not '1111'
+password = '1111'
+incorrect_password = not '1111'
+incorrect_id : print ("That is incorrect. Try again.") 
+
 print ("Weclome to the New Era Government Database. Please enter security code.")
 
+if not password print ("Incorrect. Please try again.")
